ba650590fa7147f247ff4c593647782813896d39
Mónica <serracin.monica@gmail.com>
2021-06-04 11:13:16 -0600

    Proyecto base con vue instalado

---
+++ marketplace/src/main.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios"
+import VueAxios from 'vue-axios'
+import VueAlertify from "vue-alertify"
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
+ 
+Vue.use(VueAlertify)
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ marketplace/src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header></Header>
+        <h1>{{usuario}}</h1>
+    </div>
+  
+</template>
+<script>
+import Header from "@/components/Header.vue"
+export default {
+    name: "Dashboard",
+    components:{
+        Header
+    ,
+    data: () => ({
+     
+      
+     }),
+}
+    
+}
+</script>
