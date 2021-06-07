59f7504d3a5d974652be0c7c1d91cd235c8bee36
Roberth <roberth123pz@gmail.com>
2021-06-07 00:46:37 -0600

    Crear usuario 85%

--- marketplace/src/components/UserEditor.vue
+++ marketplace/src/components/UserEditor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <h1>Editor de usuarios</h1>
-    <form class="row">
+    <form class="row" v-on:submit.prevent='saveData'>
       <div class="col-sm-6" style="background-color: #f5f5f5">
         <div>
           <label class="inputLabel" for="cedula"
@@ -13,6 +13,8 @@
           type="text"
           placeholder="Ingresa tu cédula física o jurídica"
           required
+          v-model="cedula"
+          maxlength="64"
         />
 
         <div>
@@ -25,6 +27,8 @@
           type="text"
           placeholder="Ingresa tu nombre o el de tu tienda"
           required
+          v-model="nombreCompleto"
+          maxlength="128"
         />
 
         <div>
@@ -35,6 +39,8 @@
           type="text"
           placeholder="Ingresa tu nombre o el de tu tienda"
           required
+          v-model="email"
+          maxlength="32"
         />
 
         <div><label class="inputLabel" for="telefono">Teléfono</label></div>
@@ -43,6 +49,8 @@
           type="text"
           placeholder="Ingresa tu nombre o el de tu tienda"
           required
+          v-model="telefono"
+          maxlength="16"
         />
       </div>
       <div class="col-sm-4" style="background-color: #f5f5f5">
@@ -54,6 +62,8 @@
           type="text"
           placeholder="Ingresa una contraseña segura"
           required
+          v-model="usuario"
+          maxlength="32"
         />
 
         <div><label class="inputLabel" for="password">Contraseña</label></div>
@@ -63,12 +73,36 @@
           type="password"
           placeholder="Ingresa una contraseña segura"
           required
+          v-model="password"
+          maxlength="24"
         />
         <div><label class="inputLabel">Dirección</label></div>
         <div class="form-row" style="background-color: #f5f5f5">
-          <input class="sr-only" id="pais" type="text" placeholder="País" required/>
-          <input id="provincia" type="text" placeholder="Provincia" required/>
-          <input id="canton" type="text" placeholder="Cantón" required/>
+          <input
+            maxlength="32"
+            class="sr-only"
+            id="pais"
+            type="text"
+            placeholder="País"
+            required
+            v-model="pais"
+          />
+          <input
+            maxlength="32"
+            id="provincia"
+            type="text"
+            placeholder="Provincia"
+            required
+            v-model="provincia"
+          />
+          <input
+            maxlength="32"
+            id="canton"
+            type="text"
+            placeholder="Cantón"
+            required
+            v-model="canton"
+          />
         </div>
       </div>
       <div class="col-sm-2" style="background-color: #f5f5f5">
@@ -81,10 +115,24 @@
         <div id="tipe_select">
           <h4>Tipo de usuario</h4>
           <label class="r_type" for="tienda">Tienda</label>
-          <input id="tienda" type="radio" name="usr_type" required/>
+          <input
+            name="type"
+            id="tienda"
+            type="radio"
+            value="T"
+            required
+            v-model="tipoUsario"
+          />
 
           <label class="r_type" for="cliente">Cliente</label>
-          <input id="cliente" type="radio" name="usr_type" required/>
+          <input
+            name="type"
+            id="cliente"
+            type="radio"
+            value="C"
+            required
+            v-model="tipoUsario"
+          />
         </div>
       </div>
       <div class="form-group" style="background-color: #f5f5f5">
@@ -109,10 +157,69 @@ export default {
   data: () => ({
     usuario: "",
     password: "",
-    error: false,
+    nombreCompleto: "",
+    cedula: "",
+    email: "",
+    telefono: "",
+    pais: "",
+    provincia: "",
+    canton: "",
+    tipoUsario: "",
   }),
 
-  methods: {},
+  methods: {
+    proccessAxiosError(error) {
+      if (error.response) {
+        if (error.response.status == 500 || error.response.status == 404) {
+          
+          this.$alertify.error(
+            "Han surgido problemas para conectarse con el servidor. Inténtelo más tarde."
+          );
+        } else {
+          this.$alertify.error(error.response.data);
+        }
+      } else {
+        this.$alertify.error(
+          "No se ha podido establecer conexión con el servidor."
+        );
+      }
+    },
+
+    insertData(formData) {
+      axios
+        .post(
+          process.env.VUE_APP_API_URL + "/create_usuario",
+          JSON.stringify(formData)
+        )
+        .then((response) => {
+          this.$router.push('/');
+          this.$alertify.success(response.data);
+        })
+        .catch((error) => {
+          this.proccessAxiosError(error);
+        });
+    },
+
+    saveData() {
+      let data = this.buildFormData();
+      this.insertData(data);
+    },
+
+    buildFormData() {
+      return {
+        usuario_nom_urs: this.usuario,
+        usuario_contrasena: this.password,
+        usuario_email: this.email,
+        usuario_telefono: this.telefono,
+        usuario_cedula: this.cedula,
+        usuario_nombre_compl: this.nombreCompleto,
+        usuario_tipo: this.tipoUsario,
+        direcciion_pais: this.pais,
+        direccion_provincia: this.provincia,
+        direccion_canton: this.canton,
+      };
+    },
+  },
 };
 </script>
 
